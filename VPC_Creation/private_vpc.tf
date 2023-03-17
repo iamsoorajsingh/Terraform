086c99d2538d6800d85e93d6a75b2e6d71c457f4
Suraj Singh <36132917+iamsoorajsingh@users.noreply.github.com>
2023-03-18 02:09:46 +0530

    Update private_vpc.tf

--- VPC_Creation/private_vpc.tf
+++ VPC_Creation/private_vpc.tf
@@ -3,7 +3,10 @@ provider "aws" {
   region = "ap-south-1"
 }
 resource "aws_vpc" "vpc_terrform" {
-  cidr_block = "178.150.100.0/16"
+ cidr_block = "192.168.1.0/24"
   instance_tenancy = "default"
+  tags = {
+    "Name" = "TerrformVPC"
+  }
 
 }
